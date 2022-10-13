ab6e719be5c6732b0a83668006d9fc161ac12f5a
fhgraceb <fhb190776@fh-vie.ac.at>
2022-10-13 11:03:53 +0200

    adding one tiny user test

--- TimeSheet_DB/src/test/java/com/swad/timesheet/timesheet_db/TimeSheetDbApplicationTests.java
+++ TimeSheet_DB/src/test/java/com/swad/timesheet/timesheet_db/TimeSheetDbApplicationTests.java
@@ -1,13 +1,32 @@
 package com.swad.timesheet.timesheet_db;
 
+import com.swad.timesheet.timesheet_db.entity.Project;
+import com.swad.timesheet.timesheet_db.entity.User;
+import com.swad.timesheet.timesheet_db.interfaces.ProjectRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class TimeSheetDbApplicationTests {
 
+    @Autowired
+    ProjectRepository projectRepository;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testCreateUser() {
+        projectRepository.save(Project.builder().name("MasterTestProject").build());
+        projectRepository.save(Project.builder().name("TestProject").build());
+        projectRepository.save(Project.builder().name("TinyTestProject").build());
+        System.out.println(projectRepository.count());
+        List<Project> projects = projectRepository.findAll();
+        projects.forEach(System.out::println);
     }
 
 }
