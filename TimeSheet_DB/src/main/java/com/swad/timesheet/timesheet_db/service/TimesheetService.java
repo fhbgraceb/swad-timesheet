1f48ecb78853acd5766a2eb093461649af269678
Natalie Bruckmueller <fhb201236@fh-vie.ac.at>
2022-11-06 10:57:55 +0100

    Timesheet Services implemented

---
+++ TimeSheet_DB/src/main/java/com/swad/timesheet/timesheet_db/service/TimesheetService.java
@@ -0,0 +1,14 @@
+package com.swad.timesheet.timesheet_db.service;
+
+import com.swad.timesheet.timesheet_db.entity.Project;
+import dto.TimesheetDto;
+import dto.UserDto;
+
+import java.util.List;
+
+public interface TimesheetService {
+
+    List<TimesheetDto> getTimesheetsbyUser(UserDto user);
+    List<TimesheetDto> getTimesheetsbyProject (Project project);
+
+}
